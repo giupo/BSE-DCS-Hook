16ac469231e34afe3e80d2766c45b99768419ae4
MasterOfPuppets <giuseppe.acito@bancaditalia.it>
2025-11-28 11:56:16 +0100

    Bump version: 0.0.4 → 0.1.0

--- src/config.lua
+++ src/config.lua
@@ -1,6 +1,6 @@
 local config = {
     PKGNAME = "BSE-Export",
-    VERSION = "0.0.4",
+    VERSION = "0.1.0",
 
     -- this is the address towards who we are flooding with DCS data.
     -- usually this should be our middleware that exposes data as a REST API
